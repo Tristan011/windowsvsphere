7c7daa2b49607dfc38acb8ee9f8f7b31307430f2
VanderheydenRuben <>
2021-01-05 17:18:20 +0100

    Update AfterInstall.ps1

--- AfterInstall.ps1
+++ AfterInstall.ps1
@@ -4,6 +4,8 @@ Param (
 	$count
 )
 
-Rename-Computer -NewName "Company$count" -Force
+$PlainPassword = "Pass123!"
+$SecurePassword = $PlainPassword | ConvertTo-SecureString -AsPlainText -Force
+
 Install-WindowsFeature –Name AD-Domain-Services –IncludeManagementTools
-Install-ADDSForest -DomainName "Company$count.local" -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "7" -DomainNetbiosName "Company$count" -ForestMode "7" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$True -SysvolPath "C:\Windows\SYSVOL" `-Force:$true
+Install-ADDSForest -DomainName "Company$count.local" -SafeModeAdministratorPassword $SecurePassword -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "7" -DomainNetbiosName "Company$count" -ForestMode "7" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$True -SysvolPath "C:\Windows\SYSVOL" -Force:$true
